4ed7603a02c7d7924af96d4494af5740866a55b9
redaphid <iam@aaronherres/com>
2025-04-09 01:12:14 -0700

    broken test

--- tests.mjs
+++ tests.mjs
@@ -29,13 +29,28 @@ describe("PaperCrane", () => {
       })
       it("should render a red squareexpect(render).to.exist", () => {
         const gl = canvas.getContext("webgl2")
+        const pixels = new Uint8Array(4)
+        gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+        expect(pixels).to.deep.equal(new Uint8Array([255, 0, 0, 255]))
+
+      })
+    })
+    describe("When called with a feature", () => {
+      beforeEach(() => {
+        render({fragmentShader: `
+          void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+            fragColor = vec4(0.0, blue, 0.0, 1.0);
+          }
+        `, features: { blue: 0.5 }})
+      })
+      it("should render a blue square", () => {
+        const gl = canvas.getContext("webgl2")
         const pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4)
         gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
-        expect(pixels[0]).to.equal(255)
-        expect(pixels[1]).to.equal(0)
+        expect(pixels[0]).to.equal(0)
+        expect(pixels[1]).to.equal(128)
         expect(pixels[2]).to.equal(0)
         expect(pixels[3]).to.equal(255)
-
       })
     })
   })
